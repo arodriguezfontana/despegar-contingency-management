c317075db4e8762ad6571c7adba22e39afcce8c4
Abril <abrilrodriguezfontana03@gmail.com>
2026-04-26 13:58:11 -0300

    backend setup (model, service, repository, controller and dto)

---
+++ backend/despegar-backend/src/main/java/com/example/despegarbackend/DespegarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.despegarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespegarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DespegarBackendApplication.class, args);
+    }
+
+}
---
+++ backend/despegar-backend/src/test/java/com/example/despegarbackend/DespegarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.despegarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespegarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
